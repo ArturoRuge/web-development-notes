d1a92874d773dde57685c1aac14470451bf056b0
ArturoRuge <arturoruge@gmail.com>
2024-08-20 13:03:25 -0500

    promp sync file example

---
+++ JS/ejercicios-fundamentacion/promp-sync.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+let año;
+año=1995;
+console.log(año)
+number = parseInt(prompt("Escriba un número:" ))
+total= (año + number)
+ 
+console.log(number)
+console.log(total)
